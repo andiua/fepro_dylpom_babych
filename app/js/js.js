43cd08c6fabeaa29c4c30dc05549ff15787dadb7
andiua <andr.babych@gmail.com>
2017-12-04 19:48:02 +0200

    add js

---
+++ app/js/js.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function(){
+  $('.toure__content-wrap').slick({
+  // dots: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 2500,
+   arrows: true,
+  });
+});
+
