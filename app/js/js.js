073841b75abfb3d84667574799b523d7698bad79
andiua <andr.babych@gmail.com>
2017-12-07 01:59:45 +0200

    styles for feedback block toure-list and footer

--- app/js/js.js
+++ app/js/js.js
@@ -1,11 +1,45 @@
 ﻿$(document).ready(function(){
-  $('.toure__content-wrap').slick({
+ 	$('.toure__content-wrap').slick({
   // dots: true,
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  autoplay: false,
-  autoplaySpeed: 2500,
-   arrows: true,
-  });
+	slidesToShow: 4,
+	slidesToScroll: 1,
+	autoplay: false,
+	arrows: true,	
+	responsive: [
+		{
+			breakpoint: 1299,
+			settings: {
+				slidesToShow: 3
+			}
+		},
+		{
+			breakpoint: 739,
+			settings: {
+				slidesToShow: 2,
+			}
+		},
+		{
+			breakpoint: 479,
+			settings: {
+				slidesToShow: 1,
+			}
+		}
+	]
+	});
+	$('.feedback__wrap').slick({
+  	dots: true,
+	slidesToShow: 2,
+	slidesToScroll: 1,
+	autoplay: true,
+	speed: 1000,
+	responsive: [
+		{
+			breakpoint: 479,
+			settings: {
+				slidesToShow: 1
+			}
+		}
+	]
+	});
 });
 
